69e8192b8488ae67d8b04ab65abe6b91593eb6de
SJJ <978622005@qq.com>
2017-12-26 09:33:01 +0800

    完成

--- src/main/kotlin/sjj/transmit/udp/UDPConnection.kt
+++ src/main/kotlin/sjj/transmit/udp/UDPConnection.kt
@@ -1,31 +1,34 @@
 package sjj.transmit.udp
 
 import java.io.IOException
-import java.net.DatagramPacket
-import java.net.DatagramSocket
-import java.net.InetSocketAddress
+import java.net.*
 
 class UDPConnection : ConnectInterface {
-    private val UDP_IP = "192.168.2.224"
-    private val UDP_PORT = 14550
     private val UDP_RECEIVE_PORT = 14550
     private var receive: DatagramPacket = DatagramPacket(ByteArray(4096), 4096)
     private var send: DatagramPacket = DatagramPacket(byteArrayOf(0), 0)
     private var conn: DatagramSocket? = null
+    private var socketAddress: SocketAddress? = null
+    private var address: InetAddress? = null
+    private var port: Int = 0
     override fun openConnection() {
         conn?.close()
         conn = DatagramSocket(UDP_RECEIVE_PORT)
-        send.socketAddress = InetSocketAddress(UDP_IP,UDP_PORT)
     }
 
     override fun readDataBlock(buffer: ByteArray): Int {
         val conn = conn ?: throw IOException("连接未打开")
         conn.receive(receive)
+        address = receive.address
+        port = receive.port
         System.arraycopy(receive.data, receive.offset, buffer, 0, receive.length)
         return receive.length
     }
 
     override fun sendBuffer(buffer: ByteArray) {
+        val address = address ?: return
+        send.address = address
+        send.port = port
         send.data = buffer
         conn!!.send(send)
     }
