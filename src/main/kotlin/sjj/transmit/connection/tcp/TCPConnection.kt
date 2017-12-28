df7540a713a606721c4bb9ea13f99fe4049e9087
SJJ <978622005@qq.com>
2017-12-28 11:06:33 +0800

    tcp超时设置

--- src/main/kotlin/sjj/transmit/connection/tcp/TCPConnection.kt
+++ src/main/kotlin/sjj/transmit/connection/tcp/TCPConnection.kt
@@ -8,7 +8,7 @@ class TCPConnection(private val ip: String = "192.168.2.221", private val port: 
     private lateinit var socket: Socket
     override fun openConnection() {
         socket = Socket()
-        socket.connect(InetSocketAddress(ip, port))
+        socket.connect(InetSocketAddress(ip, port),5000)
     }
 
     override fun readDataBlock(buffer: ByteArray): Int {
